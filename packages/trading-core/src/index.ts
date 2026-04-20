1a5209b66fe2a04dac3ec527715e5ddc429ab858
keletonik <meinstarken@protonmail.com>
2026-04-20 11:09:14 +1000

    chore: purge legacy packages/trading-core/src

--- packages/trading-core/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./types";
-export * from "./paper-engine";
-export * from "./portfolio";
-export * from "./alerts";
-export * from "./ledger";
-
-export const TRADING_CORE_PACKAGE = "@xake/trading-core";
-export const TRADING_CORE_STAGE = 6;
-export const DEFAULT_ENVIRONMENT = "paper" as const;
