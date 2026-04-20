73e58990163f828c634e3998725d57721846ec7f
keletonik <meinstarken@protonmail.com>
2026-04-20 11:09:23 +1000

    chore: purge legacy packages/ui/src

--- packages/ui/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./tokens";
-export * from "./lib/cn";
-export * from "./theme/theme-provider";
-export * from "./theme/theme-toggle";
-export * from "./components";
-
-export const UI_PACKAGE = "@xake/ui";
-export const UI_STAGE = 2;
