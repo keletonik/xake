cca9848396f383fb5536757a496a72d8df2ab310
keletonik <meinstarken@protonmail.com>
2026-04-20 11:09:01 +1000

    chore: purge legacy packages/data-core/src

--- packages/data-core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types";
-export * from "./normalise";
-export * from "./providers";
-export { CATALOGUE, bySymbol } from "./instruments/catalogue";
-
-export const DATA_CORE_PACKAGE = "@xake/data-core";
-export const DATA_CORE_STAGE = 6;
